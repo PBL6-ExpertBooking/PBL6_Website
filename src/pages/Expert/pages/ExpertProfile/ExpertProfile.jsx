ca1340063047dde75b8d5ddbea6b934c3025fb24
congpt <congpt@runsystem.net>
2023-10-15 12:59:45 +0700

    Intergrate API update account expert

--- src/pages/Expert/pages/ExpertProfile/ExpertProfile.jsx
+++ src/pages/Expert/pages/ExpertProfile/ExpertProfile.jsx
@@ -1,8 +1,17 @@
 import { Box, Stack, Avatar, Button, TextField, Typography, Card, FormControl, InputLabel, Select, MenuItem } from '@mui/material'
-import {React, useState } from 'react'
+import {React, useState, useEffect } from 'react'
 import { styled } from '@mui/material/styles'
 import CloudUploadIcon from '@mui/icons-material/CloudUpload'
 import CertificateInfo from '../../components/CertificateInfo'
+import useSnackbar from '../../../../contexts/snackbar.context'
+import Snackbar from '../../../../common/components/SnackBar'
+import AxiosInterceptors from '../../../../common/utils/axiosInterceptors'
+import { DateField } from '@mui/x-date-pickers/DateField';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import dayjs from 'dayjs';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import urlConfig from '../../../../config/UrlConfig'
+
 const VisuallyHiddenInput = styled('input')({
   clip: 'rect(0 0 0 0)',
   clipPath: 'inset(50%)',
@@ -16,14 +25,93 @@ const VisuallyHiddenInput = styled('input')({
 })
 
 const ExpertProfile = () => {
-  const [gender, setGender] = useState(0);
-  const [major, setMajor] = useState(0);
+  const [profile, setProfile] = useState({});
+  const [formData, setFormData] = useState(new FormData());
+  const [isValidated, setIsValidated] = useState(true)
+  const { snack, setSnack } = useSnackbar()
 
-  const handleChange = (event) => {
-    setGender(event.target.value);
-  };
+  const fetchData = async () => {
+    const res = await AxiosInterceptors.get(urlConfig.user.info)
+    if (res.status === 200) {
+      if(res.data.user) {
+        setProfile(res.data.user)
+      }
+    } else {
+      setIsValidated(false)
+    }
+  }
+
+  const handleUpdateProfile = async () => {
+    console.log("oke")
+    const res = await AxiosInterceptors.put(
+      urlConfig.user.info,
+      {
+        first_name: profile.first_name,
+        last_name: profile.last_name,
+        gender: profile.gender,
+        phone: profile.phone,
+        address: profile.address,
+        DoB: profile.DoB,
+        photo: formData.get('photo')
+      },
+      {
+        headers: {
+          'Content-Type': 'multipart/form-data'
+        }
+      }
+    )
+      .then((res) => {
+        console.log("oke")
+        console.log(res.data.user)
+        setProfile(res.data.user)
+        localStorage.setItem('profile', JSON.stringify(res.data.user))
+        setSnack({
+          open: true,
+          message: 'Update Profile Successfully',
+          type: 'success'
+        })
+      })
+      .catch((err) =>
+      {
+        console.log("fail");
+          setSnack({
+            open: true,
+            message: `${err.response.data.message}`,
+            type: 'error'
+          })
+
+      }
+      )
+  }
+
+  useEffect(() => {
+    (async () => {
+      await fetchData();
+    })();
+  }, [])
+
   return (
+    (!isValidated && (
+      <div style={{ width: '100%' }}>
+        <Card
+          sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'center',
+            alignItems: 'center',
+            height: '80vh',
+            width: '100%',
+            backgroundColor: '#f5f5f5',
+            color: 'red'
+          }}
+        >
+          <h1>Please check your email to validate your account!</h1>
+        </Card>
+      </div>
+    )) ||
+    (profile.role && (
     <div style={{width: '100%', height: '100vh'}}>
+      <Snackbar />
       <Card
         sx={{
           display: 'flex',
@@ -49,13 +137,30 @@ const ExpertProfile = () => {
               width: '50%'
             }}
           >
-            <Avatar alt='Remy Sharp' src='https://demos.themeselection.com/marketplace/materio-mui-react-nextjs-admin-template/demo-1/images/avatars/1.png' sx={{ width: 250, height: 250 }} />
-            <Box>
-              <Button component='label' variant='contained' startIcon={<CloudUploadIcon />}>
-                Upload file
-                <VisuallyHiddenInput type='file' />
-              </Button>
-            </Box>
+            <Avatar alt='Remy Sharp' src={profile.photo_url} sx={{ width: 250, height: 250 }} />
+              <Box>
+                <Button component='label' variant='contained' startIcon={<CloudUploadIcon />}>
+                  Upload Photo
+                  <VisuallyHiddenInput
+                    type='file'
+                    accept='.jpg, .png'
+                    onChange={(e) => {
+                      const file = e.target.files[0]
+                      let newFormData = new FormData()
+                      newFormData.append('photo', file)
+                      setFormData(newFormData)
+                      const reader = new FileReader()
+                      reader.readAsDataURL(file)
+                      reader.onloadend = () => {
+                        setProfile({
+                          ...profile,
+                          photo_url: reader.result
+                        })
+                      }
+                    }}
+                  />
+                </Button>
+              </Box>
           </Stack>
           <Box sx={{ display: 'block', width: '100%' }}>
             <Typography variant='h4' component='h4' sx={{ margin: '1.5rem' }}>
@@ -67,8 +172,8 @@ const ExpertProfile = () => {
                   '& .MuiTextField-root': { m: 2, width: '45%' }
                 }}
               >
-                <TextField fullWidth required id='outlined-required' label='Username' defaultValue='test123' />
-                <TextField fullWidth required id='outlined-required' label='Email' defaultValue='test123@gmail.com' />
+                <TextField fullWidth required id='outlined-required' label='Username' defaultValue={profile.username} disabled />
+                <TextField fullWidth required id='outlined-required' label='Email' defaultValue={profile.email} disabled />
               </Box>
 
               <Box
@@ -76,12 +181,32 @@ const ExpertProfile = () => {
                   '& .MuiTextField-root': { m: 2, width: '45%' }
                 }}
               >
-                <TextField required id='outlined-required' label='First Name' defaultValue='test' />
-                <TextField required id='outlined-required' label='Last Name' defaultValue='user ' />
+                <TextField 
+                  required id='outlined-required' 
+                  label='First Name' 
+                  defaultValue={profile.first_name}
+                  onChange={(e) => {
+                    setProfile({
+                      ...profile,
+                      first_name: e.target.value
+                    })
+                  }}
+                />
+                <TextField 
+                  required id='outlined-required' 
+                  label='Last Name'
+                  defaultValue={profile.last_name}
+                  onChange={(e) => {
+                    setProfile({
+                      ...profile,
+                      last_name: e.target.value
+                    })
+                  }}
+                />
               </Box>
               <Box
                 sx={{
-                  '& .MuiTextField-root': { m: 2, width: '45%' }
+                  '& .MuiTextField-root': { m: 2, width: '29%' }
                 }}
               >
                 <TextField
@@ -91,32 +216,68 @@ const ExpertProfile = () => {
                   InputLabelProps={{
                     shrink: true
                   }}
-                  defaultValue='0123456789'
+                  defaultValue={profile.phone}
+                  onChange={(e) => {
+                    setProfile({
+                      ...profile,
+                      phone: e.target.value
+                    })
+                  }}
                 />
 
                 <FormControl 
-                  sx={{width: '45%', m: 2}}
+                  sx={{width: '29%', m: 2}}
                 >
                   <InputLabel id="demo-simple-select-label">Gender</InputLabel>
                   <Select
                     labelId="demo-simple-select-label"
                     id="demo-simple-select"
-                    value={gender}
                     label="Gender"
-                    onChange={handleChange}
+                    defaultValue={profile.gender ? 1 : 0}
+                    onChange={(e) => {
+                      setProfile({
+                        ...profile,
+                        gender: e.target.value === 1 ? true : false
+                      })
+                    }}
                   >
                     <MenuItem value={0}>Male</MenuItem>
                     <MenuItem value={1}>Female</MenuItem>
-                    <MenuItem value={2}>Orther</MenuItem>
                   </Select>
                 </FormControl>
+
+                <LocalizationProvider 
+                      dateAdapter={AdapterDayjs}
+                      sx={{width: '45%', m: 2}}
+                    >
+                      <DateField
+                        label="Date of birthday"
+                        value={dayjs(profile.DoB)}
+                        onChange={(newValue) => setProfile(
+                          {
+                            ...profile,
+                            DoB: newValue
+                          }
+                        )}
+                      />
+                  </LocalizationProvider>
               </Box>
               <Box
                 sx={{
                   '& .MuiTextField-root': { m: 2, width: '94%' }
                 }}
               >
-                <TextField required id='outlined-required' label='Address' defaultValue='test' />
+                <TextField 
+                  required id='outlined-required' 
+                  label='Address'
+                  defaultValue={profile.address}
+                  onChange={(e) => {
+                    setProfile({
+                      ...profile,
+                      address: e.target.value
+                    })
+                  }}
+                />
               </Box>
             </Box>
             <Stack
@@ -128,7 +289,7 @@ const ExpertProfile = () => {
                 marginRight: '2rem'
               }}
             >
-              <Button variant='contained' component='label'>
+              <Button variant='contained' component='label' onClick={handleUpdateProfile}>
                 Save Change
               </Button>
               <Button variant='contained' component='label' color='error'>
@@ -142,6 +303,7 @@ const ExpertProfile = () => {
       {/* === */}
       <CertificateInfo/>
     </div>
+    ))
   )
 }
 
