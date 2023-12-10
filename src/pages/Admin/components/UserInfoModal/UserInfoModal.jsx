712926b63f145c4475b660e8f6379d9f2fe25138
congpt <congpt@runsystem.net>
2023-12-10 21:44:50 +0700

    update user management UI

--- src/pages/Admin/components/UserInfoModal/UserInfoModal.jsx
+++ src/pages/Admin/components/UserInfoModal/UserInfoModal.jsx
@@ -11,7 +11,8 @@ import {
   FormControl,
   InputLabel,
   Select,
-  MenuItem
+  MenuItem,
+  Grid
 } from '@mui/material'
 import { DateField } from '@mui/x-date-pickers/DateField'
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider'
@@ -22,6 +23,7 @@ import urlConfig from '../../../../config/UrlConfig'
 import useSnackbar from '../../../../contexts/snackbar.context'
 import Snackbar from '../../../../common/components/SnackBar'
 import AxiosInterceptors from '../../../../common/utils/axiosInterceptors'
+import RootModal from '../../../../components/Modal/RootModal'
 import Axios from 'axios'
 import { useTranslation } from 'react-i18next'
 
@@ -37,7 +39,7 @@ const style = {
   borderRadius: 2
 }
 
-export default function UserInfoModal({ open, handleCloseModal, user, setRerender }) {
+export default function UserInfoModal({ open, handleCloseModal, user, fetchData }) {
   const [userId, setUserId] = useState('')
   const [firstName, setFirstName] = useState('')
   const [lastName, setLastName] = useState('')
@@ -209,7 +211,7 @@ export default function UserInfoModal({ open, handleCloseModal, user, setRerende
       DoB: DoB
     })
     if (res.status === 200) {
-      setRerender()
+      fetchData()
       setSnack({
         open: true,
         message: t('updateAccountSuccess'),
@@ -225,283 +227,229 @@ export default function UserInfoModal({ open, handleCloseModal, user, setRerende
     handleCloseModal()
   }
 
-  const renderTitle = (role) => {
-    switch (role) {
-      case 'USER': {
-        return t('userProfile')
-      }
-      case 'EXPERT': {
-        return t('expertProfile')
-      }
-      case 'ADMIN': {
-        return t('adminProfile')
-      }
-    }
-  }
-
   return (
     <div>
-      <Modal
+      <RootModal
+        variant='Create'
+        title={t('userProfile')}
         open={open}
-        onClose={() => handleCloseModal()}
-        borderRadius
-        aria-labelledby='modal-modal-title'
-        aria-describedby='modal-modal-description'
+        handleClose={() => handleCloseModal()}
+        handleOk={() => {
+          handleOnclickSaveChangesBtn()
+          handleCloseModal()
+        }}
+        closeOnly={false}
       >
-        <Box sx={style}>
-          <Card
-            sx={{
-              display: 'flex',
-              flexDirection: 'row'
-            }}
-          >
-            <Stack
-              spacing={2}
-              direction='row'
-              sx={{
-                width: '100%'
+        <Grid
+          container
+          spacing={1.5}
+          sx={{
+            mt: 2
+          }}
+        >
+          <Grid item xs={12} sm={6}>
+            <TextField
+              required
+              id='outlined-required'
+              label={t('firstName')}
+              value={firstName}
+              onChange={(e) => setFirstName(e.target.value)}
+              sx={{ width: '100%'}}
+            />
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <TextField
+              required
+              id='outlined-required'
+              label={t('lastName')}
+              value={lastName}
+              sx={{ width: '100%'}}
+              onChange={(e) => setLastName(e.target.value)}
+            />
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <TextField
+              fullWidth
+              required
+              id='outlined-required'
+              label={t('username')}
+              value={username}
+              sx={{ width: '100%'}}
+              disabled
+            />
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <TextField 
+              fullWidth 
+              required 
+              id='outlined-required' 
+              label='Email' 
+              value={email} 
+              sx={{ width: '100%'}}
+              disabled />
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <TextField
+              id='outlined-number'
+              label={t('phoneNumber')}
+              type='number'
+              InputLabelProps={{
+                shrink: true
               }}
+              sx={{ width: '100%'}}
+              value={phone}
+              onChange={(e) => setPhone(e.target.value)}
+            />
+          </Grid>
+          <Grid item xs={6} sm={6}>
+            <FormControl sx={{ width: '100%' }}>
+              <InputLabel id='demo-simple-select-label'>{t('gender')}</InputLabel>
+              <Select
+                labelId='demo-simple-select-label'
+                id='demo-simple-select'
+                value={gender}
+                label={t('gender')}
+                onChange={(e) => setGender(e.target.value)}
+              >
+                <MenuItem value={0}>{t('male')}</MenuItem>
+                <MenuItem value={1}>{t('female')}</MenuItem>
+              </Select>
+            </FormControl>
+          </Grid>
+          <Grid item xs={6} sm={6}>
+            <FormControl sx={{ width: '100%'}}>
+              <InputLabel id='demo-simple-select-label'>{t('role')}</InputLabel>
+              <Select
+                labelId='demo-simple-select-label'
+                id='demo-simple-select'
+                value={role}
+                label='Role'
+                onChange={(e) => setRole(e.target.value)}
+                disabled
+              >
+                <MenuItem value={0}>{t('USER')}</MenuItem>
+                <MenuItem value={1}>{t('EXPERT')}</MenuItem>
+                <MenuItem value={2}>{t('ADMIN')}</MenuItem>
+              </Select>
+            </FormControl>
+          </Grid>
+          <Grid item xs={6} sm={6}>
+            <TextField
+              id='outlined-number'
+              label={t('verify')}
+              sx={{ width: '100%'}}
+              disabled
+              value={isConfirmed ? t('YES') : t('NO')}
+            />
+          </Grid>
+          <Grid item xs={6} sm={6}>
+            <TextField
+              id='outlined-number'
+              label={t('status')}
+              sx={{ width: '100%'}}
+              disabled
+              value={isRestricted ? t('active') : t('unactive')}
+            />
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <LocalizationProvider dateAdapter={AdapterDayjs} sx={{ width: '100%'}}>
+              <DateField
+                label={t('dateOfBirth')}
+                sx={{ width: '100%'}}
+                value={dayjs(DoB)}
+                onChange={(newValue) => setDoB(newValue)}
+              />
+            </LocalizationProvider>
+          </Grid>
+          <Grid item xs={12} sm={4}>
+            <TextField
+              id='outlined-select-currency'
+              select
+              label={t('city')}
+              defaultValue={user?.address?.city?.name}
+              sx={{ width: '100%'}}
             >
-              <Box sx={{ display: 'block', width: '100%' }}>
-                <Typography variant='h4' component='h4' sx={{ margin: '1.5rem' }}>
-                  {renderTitle(user.role).toUpperCase()}
-                </Typography>
-                <Box component='form' noValidate autoComplete='off'>
-                  <Box
-                    sx={{
-                      '& .MuiTextField-root': { m: 2, width: '45%' }
+              {tinh &&
+                tinh.length > 0 &&
+                tinh?.map((option) => (
+                  <MenuItem
+                    key={option.code}
+                    value={option.name}
+                    onClick={(e) => {
+                      setAddress({
+                        ...address,
+                        city: {
+                          code: option.code,
+                          name: option.name
+                        }
+                      })
                     }}
                   >
-                    <TextField
-                      required
-                      id='outlined-required'
-                      label={t('firstName')}
-                      value={firstName}
-                      onChange={(e) => setFirstName(e.target.value)}
-                    />
-                    <TextField
-                      required
-                      id='outlined-required'
-                      label={t('lastName')}
-                      value={lastName}
-                      onChange={(e) => setLastName(e.target.value)}
-                    />
-                  </Box>
-
-                  <Box
-                    sx={{
-                      '& .MuiTextField-root': { m: 2, width: '45%' }
-                    }}
-                  >
-                    <TextField
-                      fullWidth
-                      required
-                      id='outlined-required'
-                      label={t('username')}
-                      value={username}
-                      disabled
-                    />
-                    <TextField fullWidth required id='outlined-required' label='Email' value={email} disabled />
-                  </Box>
-
-                  <Box
-                    sx={{
-                      '& .MuiTextField-root': { m: 2, width: '45%' }
-                    }}
-                  >
-                    <TextField
-                      id='outlined-number'
-                      label={t('phoneNumber')}
-                      type='number'
-                      InputLabelProps={{
-                        shrink: true
-                      }}
-                      value={phone}
-                      onChange={(e) => setPhone(e.target.value)}
-                    />
-
-                    <FormControl sx={{ width: '45%', m: 2 }}>
-                      <InputLabel id='demo-simple-select-label'>{t('gender')}</InputLabel>
-                      <Select
-                        labelId='demo-simple-select-label'
-                        id='demo-simple-select'
-                        value={gender}
-                        label={t('gender')}
-                        onChange={(e) => setGender(e.target.value)}
-                      >
-                        <MenuItem value={0}>{t('male')}</MenuItem>
-                        <MenuItem value={1}>{t('female')}</MenuItem>
-                      </Select>
-                    </FormControl>
-                  </Box>
-
-                  <Box
-                    sx={{
-                      '& .MuiTextField-root': { m: 2, width: '45%' }
-                    }}
-                  >
-                    <FormControl sx={{ width: '45%', m: 2 }}>
-                      <InputLabel id='demo-simple-select-label'>{t('role')}</InputLabel>
-                      <Select
-                        labelId='demo-simple-select-label'
-                        id='demo-simple-select'
-                        value={role}
-                        label='Role'
-                        onChange={(e) => setRole(e.target.value)}
-                        disabled
-                      >
-                        <MenuItem value={0}>{t('USER')}</MenuItem>
-                        <MenuItem value={1}>{t('EXPERT')}</MenuItem>
-                        <MenuItem value={2}>{t('ADMIN')}</MenuItem>
-                      </Select>
-                    </FormControl>
-
-                    <TextField
-                      id='outlined-number'
-                      label={t('verify')}
-                      disabled
-                      value={isConfirmed ? t('YES') : t('NO')}
-                    />
-                  </Box>
-
-                  <Box
-                    sx={{
-                      '& .MuiTextField-root': { m: 2, width: '45%' }
-                    }}
-                  >
-                    <LocalizationProvider dateAdapter={AdapterDayjs} sx={{ width: '45%', m: 2 }}>
-                      <DateField
-                        label={t('dateOfBirth')}
-                        value={dayjs(DoB)}
-                        onChange={(newValue) => setDoB(newValue)}
-                      />
-                    </LocalizationProvider>
-
-                    <TextField
-                      id='outlined-number'
-                      label={t('status')}
-                      disabled
-                      value={isRestricted ? t('active') : t('unactive')}
-                    />
-                  </Box>
-
-                  <Stack direction='row' spacing={3} sx={{ my: 2, ml: 2 }}>
-                    <TextField
-                      id='outlined-select-currency'
-                      select
-                      label={t('city')}
-                      defaultValue={address?.city?.name}
-                      sx={{
-                        width: '30%'
-                      }}
-                    >
-                      {tinh &&
-                        tinh.length > 0 &&
-                        tinh?.map((option) => (
-                          <MenuItem
-                            key={option.code}
-                            value={option.name}
-                            onClick={(e) => {
-                              setAddress({
-                                ...address,
-                                city: {
-                                  code: option.code,
-                                  name: option.name
-                                }
-                              })
-                            }}
-                          >
-                            {option.name}
-                          </MenuItem>
-                        ))}
-                    </TextField>
-                    <TextField
-                      id='outlined-select-currency'
-                      select
-                      label={t('district')}
-                      defaultValue={address?.district?.name}
-                      sx={{
-                        width: '30%'
-                      }}
-                    >
-                      {huyen &&
-                        huyen.length > 0 &&
-                        huyen?.map((option) => (
-                          <MenuItem
-                            key={option.code}
-                            value={option.name}
-                            onClick={(e) => {
-                              setAddress({
-                                ...address,
-                                district: {
-                                  code: option.code,
-                                  name: option.name
-                                }
-                              })
-                            }}
-                          >
-                            {option.name}
-                          </MenuItem>
-                        ))}
-                    </TextField>
-                    <TextField
-                      id='outlined-select-currency'
-                      select
-                      label={t('ward')}
-                      defaultValue={address?.ward?.name}
-                      sx={{
-                        width: '30%'
-                      }}
-                    >
-                      {xa &&
-                        xa.length > 0 &&
-                        xa?.map((option) => (
-                          <MenuItem
-                            key={option.code}
-                            value={option.name}
-                            onClick={(e) => {
-                              setAddress({
-                                ...address,
-                                ward: {
-                                  code: option.code,
-                                  name: option.name
-                                }
-                              })
-                            }}
-                          >
-                            {option.name}
-                          </MenuItem>
-                        ))}
-                    </TextField>
-                  </Stack>
-                </Box>
-              </Box>
-            </Stack>
-          </Card>
-          <Stack
-            spacing={1}
-            direction='row'
-            alignItems='center'
-            justifyContent='flex-end'
-            sx={{
-              marginRight: '2rem'
-            }}
-          >
-            <Button variant='contained' component='label' sx={{ width: 130 }} onClick={handleOnclickSaveChangesBtn}>
-              {t('saveChanges')}
-            </Button>
-            <Button
-              variant='contained'
-              component='label'
-              color='error'
-              sx={{ width: 130 }}
-              onClick={() => handleCloseModal()}
+                    {option.name}
+                  </MenuItem>
+                ))}
+            </TextField>
+          </Grid>
+          <Grid item xs={12} sm={4}>
+            <TextField
+              id='outlined-select-currency'
+              select
+              label={t('district')}
+              defaultValue={user?.address?.district?.name}
+              sx={{ width: '100%'}}
             >
-              {t('cancel')}
-            </Button>
-          </Stack>
-        </Box>
-      </Modal>
+              {huyen &&
+                huyen.length > 0 &&
+                huyen?.map((option) => (
+                  <MenuItem
+                    key={option.code}
+                    value={option.name}
+                    onClick={(e) => {
+                      setAddress({
+                        ...address,
+                        district: {
+                          code: option.code,
+                          name: option.name
+                        }
+                      })
+                    }}
+                  >
+                    {option.name}
+                  </MenuItem>
+                ))}
+            </TextField>
+          </Grid>
+          <Grid item xs={12} sm={4}>
+            <TextField
+              id='outlined-select-currency'
+              select
+              label={t('ward')}
+              defaultValue={user?.address?.ward?.name}
+              sx={{ width: '100%'}}
+            >
+              {xa &&
+                xa.length > 0 &&
+                xa?.map((option) => (
+                  <MenuItem
+                    key={option.code}
+                    value={option.name}
+                    onClick={(e) => {
+                      setAddress({
+                        ...address,
+                        ward: {
+                          code: option.code,
+                          name: option.name
+                        }
+                      })
+                    }}
+                  >
+                    {option.name}
+                  </MenuItem>
+                ))}
+            </TextField>
+          </Grid>
+        </Grid>
+      </RootModal>
     </div>
   )
 }
--- src/pages/Admin/pages/UsersManagement/UsersManagement.jsx
+++ src/pages/Admin/pages/UsersManagement/UsersManagement.jsx
@@ -1,303 +1,47 @@
-import { useState, useEffect, lazy } from 'react'
-// @mui
-import { Stack, Container, IconButton, Popover, MenuItem, Typography } from '@mui/material'
-
-import { DataGrid, useGridApiRef } from '@mui/x-data-grid'
-import Label from '../../../../components/Label'
-
-import MoreVertIcon from '@mui/icons-material/MoreVert'
-import EditIcon from '@mui/icons-material/Edit'
-import DeleteIcon from '@mui/icons-material/Delete'
-import Snackbar from '../../../../common/components/SnackBar'
+import React from 'react'
 import AxiosInterceptors from '../../../../common/utils/axiosInterceptors'
-import useSnackbar from '../../../../contexts/snackbar.context'
+import UsersTable from './UsersTable'
 import urlConfig from '../../../../config/UrlConfig'
-import _ from 'lodash'
-import LockIcon from '@mui/icons-material/Lock'
 import { Helmet } from 'react-helmet-async'
 import { useTranslation } from 'react-i18next'
 import useResponsive from '../../../../hooks/useResponsive'
-
-const UserInfoModal = lazy(() => import('../../components/UserInfoModal'))
+import Loading from '../../../../common/components/Loading/Loading'
 
 const UsersManagement = () => {
   const isMobile = useResponsive('down', 'sm')
-  //STATE
-  const [openMenu, setOpenMenu] = useState(null)
-  const [currentRow, setCurrentRow] = useState(null)
-  const [openModal, setOpenModal] = useState(false)
-  const { snack, setSnack } = useSnackbar()
-  const [rerender, setRerender] = useState(true)
-  const [users, setUsers] = useState([])
-  const [loading, setLoading] = useState(false)
-  const [totalDocs, setTotalDocs] = useState(1)
   const { t } = useTranslation()
+  const [users, setUsers] = React.useState([])
+  const [isLoading, setIsLoading] = React.useState(true)
+  const [refresh, setRefresh] = React.useState(false)
 
-  const fetchUsers = async (limit = 100000) => {
+	const fetchData = async (limit = 100000) => {
     const res = await AxiosInterceptors.get(urlConfig.user.users + `?limit=${limit}`)
     if (res && res.status === 200) {
       if (res.data.pagination) {
         if (res.data.pagination.users) {
           setUsers(res.data.pagination.users)
-          setTotalDocs(res.data.pagination.totalDocs)
+          setIsLoading(false)
         }
       }
     }
   }
 
-  useEffect(() => {
-    // Gọi API để lấy danh sách người dùng ở đây
-    ;(async () => {
-      setLoading(true)
-      if (rerender) {
-        await fetchUsers()
-        setRerender(false)
-      }
-      setLoading(false)
-    })()
-  }, [rerender])
-
-  //HANDLE
-  const handleOpenMenu = (event, row) => {
-    setCurrentRow(row)
-    setOpenMenu(event.currentTarget)
-  }
-
-  const handleCloseMenu = () => {
-    setOpenMenu(null)
-  }
-
-  const handleClickEditBtn = () => {
-    setOpenModal(true)
-  }
-
-  const handleSetRerender = () => {
-    setRerender(true)
-  }
-
-  const handleClickDeleteBtn = () => {}
-
-  const handleCloseModal = () => {
-    setOpenModal(false)
-  }
-
-  const hanldeClickLockAccount = async () => {
-    handleCloseMenu()
-    if (currentRow.isRestricted) {
-      const res = await AxiosInterceptors.put(`${urlConfig.user.users}/${currentRow._id}/enable`)
-      if (res.status === 200) {
-        setRerender(true)
-        setSnack({
-          open: true,
-          message: t('unlockedAccountSuccess'),
-          type: 'success'
-        })
-      } else {
-        setSnack({
-          open: true,
-          message: t('unlockedAccountFail'),
-          type: 'error'
-        })
-      }
-    } else if (!currentRow.isRestricted) {
-      const res = await AxiosInterceptors.put(`${urlConfig.user.users}/${currentRow._id}/disable`)
-      if (res.status === 200) {
-        setRerender(true)
-        setSnack({
-          open: true,
-          message: t('lockedAccountSuccess'),
-          type: 'success'
-        })
-      } else {
-        setSnack({
-          open: true,
-          message: t('lockedAccountFail'),
-          type: 'error'
-        })
-      }
-    }
-  }
-
-  const getLabel = (item) => {
-    const map = {
-      USER: {
-        text: 'USER',
-        color: 'info'
-      },
-      EXPERT: {
-        text: 'EXPERT',
-        color: 'success'
-      },
-      ADMIN: {
-        text: 'ADMIN',
-        color: 'warning'
-      },
-      YES: {
-        text: 'YES',
-        color: 'success'
-      },
-      NO: {
-        text: 'NO',
-        color: 'error'
-      },
-      ACTIVE: {
-        text: 'ACTIVE',
-        color: 'success'
-      },
-      UNACTIVE: {
-        text: 'UNACTIVE',
-        color: 'error'
-      }
-    }
-
-    const { text, color } = map[item]
-
-    return <Label color={color}>{text}</Label>
-  }
-
-  const columns = [
-    {
-      field: 'fullName',
-      headerName: t('fullName'),
-      flex: 2,
-      renderCell: (params) => {
-        return params.row.first_name + ' ' + params.row.last_name
-      }
-    },
-    { field: 'email', headerName: 'Email', flex: 2 },
-    {
-      field: 'address',
-      headerName: t('address'),
-      flex: 5,
-      renderCell: (params) => {
-        return `${params?.row?.address?.ward?.name} - ${params?.row?.address?.district?.name} - ${params?.row?.address?.city?.name}`
-      }
-    },
-    { field: 'phone', headerName: t('phoneNumber'), flex: 1.3 },
-    {
-      field: 'role',
-      headerName: t('role'),
-      headerAlign: 'center',
-      align: 'center',
-      flex: 1,
-      renderCell: (params) => {
-        return getLabel(params.row.role)
-      }
-    },
-    {
-      field: 'isConfirmed',
-      headerName: t('verify'),
-      align: 'center',
-      justifyContent: 'center',
-      headerAlign: 'center',
-      flex: 1,
-      renderCell: (params) => {
-        return params.row.isConfirmed === true ? getLabel('YES') : getLabel('NO')
-      }
-    },
-    {
-      field: 'isRestricted',
-      headerName: t('status'),
-      headerAlign: 'center',
-      align: 'center',
-      flex: 1,
-      renderCell: (params) => {
-        return params.row.isRestricted ? getLabel('UNACTIVE') : getLabel('ACTIVE')
-      }
-    },
-    {
-      field: 'action',
-      headerName: '',
-      flex: 1,
-      sortable: false,
-      disableClickEventBubbling: true,
-
-      renderCell: (params) => {
-        return (
-          <IconButton size='large' color='inherit' onClick={(e) => handleOpenMenu(e, params.row)}>
-            <MoreVertIcon />
-          </IconButton>
-        )
-      }
-    }
-  ]
-
+  React.useEffect(() => {
+    fetchData()
+  }, [refresh])
   return (
-    <>
-      <Snackbar />
+    <div
+      style={
+        isMobile
+          ? { width: '100%', padding: '20px 20px', maxHeight: '93vh', overflow: 'auto' }
+          : { width: '100%', padding: '20px 100px', maxHeight: '93vh', overflow: 'auto' }
+      }
+    >
       <Helmet>
         <title>{t('userManagement')}</title>
       </Helmet>
-      <Container sx={isMobile ? {} : { minWidth: 1500 }}>
-        <Stack direction='row' alignItems='center' justifyContent='space-between' mb={3}>
-          <Typography variant='h3' gutterBottom>
-            {t('userManagement')}
-          </Typography>
-        </Stack>
-        <div style={{ height: 700, width: '100%' }}>
-          <DataGrid
-            getRowHeight={() => 'auto'}
-            rows={users}
-            columns={columns}
-            sx={{
-              '& .MuiDataGrid-row': {
-                minHeight: '64px !important'
-              }
-            }}
-            initialState={{
-              pagination: { paginationModel: { pageSize: 20 } }
-            }}
-            pageSizeOptions={[10, 20, 30, 50]}
-            rowCount={totalDocs}
-            paginationMode='client'
-            loading={loading}
-          />
-        </div>
-      </Container>
-
-      <Popover
-        open={Boolean(openMenu)}
-        anchorEl={openMenu}
-        onClose={handleCloseMenu}
-        anchorOrigin={{ vertical: 'top', horizontal: 'left' }}
-        transformOrigin={{ vertical: 'top', horizontal: 'right' }}
-        PaperProps={{
-          sx: {
-            p: 1,
-            width: 140,
-            '& .MuiMenuItem-root': {
-              px: 1,
-              typography: 'body2',
-              borderRadius: 0.75
-            }
-          }
-        }}
-      >
-        <MenuItem onClick={handleClickEditBtn}>
-          <EditIcon sx={{ mr: 1 }} />
-          {t('edit')}
-        </MenuItem>
-
-        <MenuItem sx={{ color: 'warning.main' }} onClick={hanldeClickLockAccount}>
-          <LockIcon sx={{ mr: 1 }} />
-          {currentRow && currentRow.isRestricted ? t('unlock') : t('lock')}
-        </MenuItem>
-
-        <MenuItem sx={{ color: 'error.main' }} onClick={handleClickDeleteBtn}>
-          <DeleteIcon sx={{ mr: 1 }} />
-          {t('delete')}
-        </MenuItem>
-      </Popover>
-
-      {currentRow && (
-        <UserInfoModal
-          open={openModal}
-          handleCloseModal={handleCloseModal}
-          user={currentRow}
-          setRerender={handleSetRerender}
-        />
-      )}
-    </>
+      {isLoading ? <Loading /> : <UsersTable users={users} fetchData={fetchData} />}
+    </div>
   )
 }
 
